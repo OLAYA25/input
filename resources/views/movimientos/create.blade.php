1e651d9237f9afd081d4a51d98f35de53f3809ff
JaviiLozano <94941083+JaviiLozano@users.noreply.github.com>
2024-08-11 23:10:21 -0500

    Update create.blade.php

--- resources/views/movimientos/create.blade.php
+++ resources/views/movimientos/create.blade.php
@@ -17,6 +17,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap4.min.css">
     <style>
         @media (max-width: 768px) {
             .dt-buttons {
